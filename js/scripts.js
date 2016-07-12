145a49bd3bc778b7d5262bc9e84224fb3ca6ac0b
Jordan Stringer and Scott Scholz <scott@>
2016-07-11 20:36:20 -0700

    WIP Pig Dice Game and included base project files.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// function that delivers a random number.
+
+var rollValue = function randomNumber() {
+  return Math.floor(Math.random() * (6)) + 1;
+}
+
+function Player(name, score, turn) {
+  this.name = name;
+  this.score = score;
+  this.turn = turn;
+}
+
+// var rollValue =
+
+var player1 = new Player("Player One", 0, true)
+var player2 = new Player("Player Two", 0, false)
+
+
+
+$(document).ready(function() {
+  $("form#some-form").click(function(event) {
+  console.log(rollValue());
+  event.preventDefault();
+  });
+});
